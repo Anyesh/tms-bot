b23b7ee4c538702e97b72dd718c3c8a32be05e87
Anyesh <sthacruz@gmail.com>
2021-09-05 11:46:59 +0545

    feat: Add new settings for loguru

--- tms/settings.py
+++ tms/settings.py
@@ -20,7 +20,7 @@ logger.add(
 logger.add(
     sys.stdout,
     colorize=True,
-    format="<green>{time}</green> <level>{message}</level>",
+    format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
     level="INFO",
 )
 
